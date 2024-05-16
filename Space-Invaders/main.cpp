4abe7578d8cc4df54f0e7847ae0f1f1d035db930
Annu Kushwah <annuetw143@gmail.com>
2024-05-16 15:45:23 +0530

    Change Window size make  theme Fullscreen

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -2,11 +2,14 @@
 
 int main() {
 
-    // Define the video mode (dimensions)
-    sf::VideoMode videoMode = *(new sf::VideoMode(800, 600));
+    
+    sf::VideoMode videoMode = *(new sf::VideoMode(1920,1080));
 
     // Create a window object with specific dimensions and a title
-    sf::RenderWindow* window = new sf::RenderWindow(videoMode, "My SFML Window");
+    sf::RenderWindow* window = new sf::RenderWindow(videoMode.getFullscreenModes()[0], "My SFML Window",sf::Style::Fullscreen);
+    window->setFramerateLimit(60);
+    window->setPosition(sf::Vector2i(100, 100));
+   
 
     // Game loop to keep the window open
     while (window->isOpen()) {
