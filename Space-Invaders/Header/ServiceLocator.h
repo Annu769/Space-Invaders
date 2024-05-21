7796cb9156373f9d25610a41fc07da3cc509bf80
Annu Kushwah <annuetw143@gmail.com>
2024-05-21 12:39:01 +0530

    Added Servicelocator header and Cpp file

---
+++ Space-Invaders/Header/ServiceLocator.h
@@ -0,0 +1,41 @@
+#pragma once
+// Service Locator Class summary this class manages acces to various services in the application
+class ServiceLocator
+{
+private :
+	ServiceLocator()
+	{
+
+	}
+	~ServiceLocator()
+	{
+
+	}
+	void createService()
+	{
+
+	}
+	void clearALlService()
+	{
+
+	}
+public :
+	//public Methods:
+	static ServiceLocator* getInstance()
+	{
+		//provide a method to acces the unique ServiceLocator instance (object ).
+	}
+	void initialize()
+	{
+		//Initializes the service locator.
+	}
+	void Update()
+	{
+		//Update all service
+	}
+	void render()
+	{
+		//render using the service.
+	}
+
+};
---
+++ Space-Invaders/Source/ServiceLocator.cpp
@@ -0,0 +1,35 @@
+#include"../Header/ServiceLocator.h"
+#include <iostream>
+
+ ServiceLocator::ServiceLocator()
+{
+
+}
+ ServiceLocator:: ~ServiceLocator()
+ {
+
+ }
+ void ServiceLocator::createService()
+ {
+
+ }
+ void ServiceLocator::clearALlService()
+ {
+
+ }
+ ServiceLocator *ServiceLocator::getInstance()
+ {
+	
+ }
+ void ServiceLocator::initialize()
+ {
+
+ }
+ void ServiceLocator::Update()
+ {
+
+ }
+ void ServiceLocator::render()
+ {
+
+ }
