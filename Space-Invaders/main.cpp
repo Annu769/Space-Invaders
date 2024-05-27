ef4f99e6c9c5f4905337fddd0bccd3445d410f53
Annu Kushwah <annuetw143@gmail.com>
2024-05-27 17:04:09 +0530

    try to fix issue

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #include"../../Header/Main/GameService.h"
 #include <iostream>
 #include <SFML/Graphics.hpp>
@@ -18,3 +19,35 @@ using namespace std;
       delete gameService;
       return 0;
   }
+=======
+#include <SFML/Graphics.hpp>
+
+int main() {
+
+    // Define the video mode (dimensions)
+    sf::VideoMode videoMode = *(new sf::VideoMode(800, 600));
+
+    // Create a window object with specific dimensions and a title
+    sf::RenderWindow* window = new sf::RenderWindow(videoMode, "My SFML Window");
+
+    // Game loop to keep the window open
+    while (window->isOpen()) {
+        sf::Event event;
+        while (window->pollEvent(event)) {
+            // Check for window closure
+            if (event.type == sf::Event::Closed)
+                window->close();
+        }
+
+        // Clear the window
+        window->clear(sf::Color::Blue);
+
+        // Draw your content here...
+
+        // Display what was drawn
+        window->display();
+    }
+
+    return 0;
+}
+>>>>>>> Stashed changes
